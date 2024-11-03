3d5e03c75b45a7ec39658413dbc65708ad18e014
DIPANKAR MAJEE <109693037+dipankarmajee@users.noreply.github.com>
2024-11-03 09:59:45 +0530

    Update preloader-words.js

--- app/_data/preloader-words.js
+++ app/_data/preloader-words.js
@@ -1,11 +1,18 @@
 export const preloaderWords = [
-  'Hello',
-  'Bonjour',
-  'Ciao',
-  'Olà',
-  'سلام',
-  'やあ',
-  'Hallå',
-  'Guten tag',
-  'Hallo',
+  'Hello',      // English
+  'স্বাগতম',    // Bengali
+  'స్వాగతం',    // Telugu
+  'வணக்கம்',     // Tamil
+  // 'नमस्कार',    // Marathi
+  // 'સ્વાગત છે',  // Gujarati
+  'ಸ್ವಾಗತ',     // Kannada
+  // 'സ്വാഗതം',    // Malayalam
+ // 'ਸੁਆਗਤ ਹੈ',   // Punjabi
+ // 'ସ୍ୱାଗତ',     // Odia
+ // 'স্বাগত',     // Assamese
+ // 'خوش آمدید',  // Urdu
+  'Hola',      // Spanish
+  'Bonjour',   // French
+  'Ciao',      // Italian
+  'नमस्ते',     // Hindi
 ];
